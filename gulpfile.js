4e7954a3a3062d7768383f0b0f065455232d3f93
JIANSO <dothewillof@naver.com>
2020-03-06 18:01:45 +0900

    4th commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
-var {src , dest, series } = require('gulp');
+var {src , dest, series, parallel, watch } = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del'); // dist 폴더에 잘못 만들어진 파일,, 업데이트 시 지워지도록. 
 
-function test(){
-    return src("./*.html").pipe(dest("./dist/"));
+function clean(){
+    return del('./dist/');
 }
 
-exports.default = series(test);
-//module.exports = series([test]);
+function test(){
+    return src("./src/*.html").pipe(dest("./dist/"));
+}
+function js(){
+    return src("./src/*.js").pipe(dest("./dist/"));
+}
+function sassf(){
+    return src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(dest('./dist/css/'));
+}
+
+function watchFiles(){
+    watch("./src/*.html", test); //html이 바꿨는지 확인하고, test를 돌리겠다. 
+    watch("./src/*.js", js); 
+    watch("./src/scss/**/*.scss", sassf); 
+}
+
+exports.clean = series(clean);
+exports.build = series(clean, test, sassf, js)
+exports.default = parallel(watchFiles, series(test,sassf, js));
+// series 는 한 줄로 지켜보는 거니까... 
+// 병렬로 수행시켜야 함. 진행이 안 되니까....  
+
